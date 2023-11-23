243034fcd77cb741f1793adf5c7cf53a70c3d11d
Yaplupile <yassine.mahjoub2001@gmail.com>
2023-11-23 17:34:00 +0100

    include gravity !

--- js/player.js
+++ js/player.js
@@ -8,29 +8,38 @@ export default class player{
 
     createPlayer = function (scene) {
         var player = BABYLON.MeshBuilder.CreateBox("player", { size: 2 }, scene);
+        player.speed = 0.2;
         player.position.y = 2; // Set the height of the player
+        player.checkCollisions = true;
+        player.grounded = true;
+        player.physicsImpostor = new BABYLON.PhysicsImpostor(player, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1 }, this.scene);
         return player;
     };
 
     movePlayer = function (player,inputStates) {
         if (inputStates.left) {
-            player.position.x += 0.1;
+            player.moveWithCollisions(new BABYLON.Vector3(-player.speed, 0, 0));
         }
         if (inputStates.right) {
-            player.position.x -= 0.1;
+            player.moveWithCollisions(new BABYLON.Vector3(player.speed, 0, 0));
         }
         if (inputStates.up) {
-            player.position.z -= 0.1;
+            player.moveWithCollisions(new BABYLON.Vector3(0, 0, player.speed));
         }
         if (inputStates.down) {
-            player.position.z += 0.1;
+            player.moveWithCollisions(new BABYLON.Vector3(0, 0, -player.speed));
         }
-        if (inputStates.space) {
-            player.position.y += 0.1;
+        if (inputStates.space && player.grounded==true) {
+            player.grounded = false;
+            player.moveWithCollisions(new BABYLON.Vector3(0, player.speed*10, 0));
+        }
+        if (player.position.y < 2.1) {
+            player.grounded = true;  
         }
    }
 
-   applyGravity = function (player) {
-        player.position.y -= 0.1;
+
+   updatePlayer = function (player,inputStates) {
+        this.movePlayer(player,inputStates);
    }
 }
--- js/scene.js
+++ js/scene.js
@@ -10,17 +10,20 @@ export default class scene{
     
     createScene = function () {
         var scene = new BABYLON.Scene(this.engine);
+        scene.enablePhysics(null, new BABYLON.CannonJSPlugin());
         this.playerManager = new player(this.canvas, this.engine,scene)
         this.player = this.playerManager.player;
         this.cameraManager = new camera(this.canvas, scene)
         this.camera = this.cameraManager.createCameraV2(scene);
         var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
         var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 100, height: 100 }, scene);
+        ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
+        ground.checkCollisions = true;
         return scene;
     };
 
     updateScene = function (scene,inputStates) {
         scene.render();
-        this.playerManager.movePlayer(this.player,inputStates);
+        this.playerManager.updatePlayer(this.player,inputStates);
     };
 }
